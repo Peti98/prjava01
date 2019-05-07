2cbf4b05b9ce7e0063d3aef799212286866cdd0d
APP <APP@SRV-TI2>
2019-05-07 17:43:57 +0200

    Versió 1.1 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -19,7 +19,7 @@ public static void main(String[] args) throws IOException {
     bw.newLine(); 
     bw.write("  <head>");bw.newLine(); bw.write("    <title>");bw.newLine();
     bw.write("      Nova p&agrave;gina  web");bw.newLine();
-bw.write("    </title>");bw.newLine(); bw.write("  </head>");bw.newLine(); bw.write("  <body>");bw.newLine();
+bw.write("    </title>");bw.newLine(); bw.write("  </head>");bw.newLine(); bw.write("  <body>");bw.write("    <h1>DAW2 m08uf4pr2</h1>");bw.newLine();bw.newLine();
 bw.write("    Nova p&agrave;gina web");bw.newLine(); bw.write("  </body>");bw.newLine(); bw.write("</html>");bw.newLine();
 bw.close();
 }
